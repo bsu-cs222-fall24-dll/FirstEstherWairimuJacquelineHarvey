5b353e844d3e15ab5e90fe96f64e139207991889
Estherwairimu <100499476+Estherwairimu@users.noreply.github.com>
2024-09-18 18:19:08 -0400

    Update Model.java
    added code to model class

--- src/test/edu.bsu.cs/Model.java
+++ src/test/edu.bsu.cs/Model.java
@@ -1,4 +1,18 @@
 package edu.bsu.cs;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Model {
+  private List<WikiChange> changes = new ArrayList<>();
+
+    public void fetchChanges(String articleName) {
+        // Implement fetching logic here, populate the changes list
+    }
+
+    public List<WikiChange> getChanges() {
+        return changes;
+    }
+
+    public void addChange(WikiChange change) {
+        changes.add(change);
+    }
 }
